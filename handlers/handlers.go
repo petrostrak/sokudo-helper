0e7b7b0caab580db3523403e5a3d07793c0d2cad
petrostrak <pit.trak@gmail.com>
2022-04-23 22:19:54 +0300

    Add sessioTest

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/CloudyKit/jet/v6"
 	"github.com/petrostrak/sokudo"
 )
 
@@ -26,6 +27,22 @@ func (h *Handlers) GoPage(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handlers) JetPage(w http.ResponseWriter, r *http.Request) {
 	if err := h.App.Render.JetPage(w, r, "jet-template", nil, nil); err != nil {
+		h.App.ErrorLog.Println("error rendering:", err)
+	}
+
+}
+
+func (h *Handlers) SessioTest(w http.ResponseWriter, r *http.Request) {
+	myData := "bar"
+
+	h.App.Session.Put(r.Context(), "foo", myData)
+
+	myValue := h.App.Session.GetString(r.Context(), "foo")
+
+	vars := make(jet.VarMap)
+	vars.Set("foo", myValue)
+
+	if err := h.App.Render.JetPage(w, r, "sessions", vars, nil); err != nil {
 		h.App.ErrorLog.Println("error rendering:", err)
 	}
 
