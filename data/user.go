e2fb7d4090bb79cf5cc2982985df8eeb1d5df145
petrostrak <pit.trak@gmail.com>
2022-04-27 17:41:31 +0300

    Add GetAll - users with upper ORM

--- data/user.go
+++ data/user.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	up "github.com/upper/db/v4"
+)
 
 type User struct {
 	ID        int       `db:"id,omitempty"`
@@ -12,4 +16,21 @@ type User struct {
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
 	Token     Token     `db:"-"`
+}
+
+func (u *User) Table() string {
+	return "users"
+}
+
+func (u *User) GetAll(condition up.Cond) ([]*User, error) {
+	collection := upper.Collection(u.Table())
+
+	var all []*User
+
+	res := collection.Find(condition)
+	if err := res.All(&all); err != nil {
+		return nil, err
+	}
+
+	return all, nil
 }
