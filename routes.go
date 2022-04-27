94c6f02ec9a82840d964efe27fcb014519a6db8b
petrostrak <pit.trak@gmail.com>
2022-04-27 10:38:50 +0300

    Trying out our database connection

--- routes.go
+++ routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -14,6 +15,27 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessioTest)
+
+	a.App.Routes.Get("/test-database", func(w http.ResponseWriter, r *http.Request) {
+		query := "select id, first_name from users where id = 1"
+		row := a.App.DB.Pool.QueryRowContext(r.Context(), query)
+
+		var user struct {
+			id   int
+			name string
+		}
+		err := row.Scan(
+			&user.id,
+			&user.name,
+		)
+
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+
+		fmt.Fprintf(w, "%d %s", user.id, user.name)
+	})
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
