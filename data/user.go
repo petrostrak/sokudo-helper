e9879494579a7cd0fdb33e7aec5fd86a9a781f6f
petrostrak <pit.trak@gmail.com>
2022-04-28 17:24:06 +0300

    Fix type in token.go and refactor GetAll

--- data/user.go
+++ data/user.go
@@ -22,12 +22,12 @@ func (u *User) Table() string {
 	return "users"
 }
 
-func (u *User) GetAll(condition up.Cond) ([]*User, error) {
+func (u *User) GetAll() ([]*User, error) {
 	collection := upper.Collection(u.Table())
 
 	var all []*User
 
-	res := collection.Find(condition)
+	res := collection.Find().OrderBy("last_name")
 	if err := res.All(&all); err != nil {
 		return nil, err
 	}
