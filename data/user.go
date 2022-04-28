50c26cdd0ebeab7bfdbb3f32a5a9c4a73d8a809f
petrostrak <pit.trak@gmail.com>
2022-04-28 17:26:44 +0300

    Add Get user by id

--- data/user.go
+++ data/user.go
@@ -60,3 +60,28 @@ func (u *User) GetByEmail(email string) (*User, error) {
 
 	return &theUser, nil
 }
+
+func (u *User) Get(id int) (*User, error) {
+	var theUser User
+	collection := upper.Collection(u.Table())
+	res := collection.Find(up.Cond{"id =": id})
+
+	err := res.One(&theUser)
+	if err != nil {
+		return nil, err
+	}
+
+	var token Token
+	collection = upper.Collection(token.Table())
+	res = collection.Find(up.Cond{"user_id =": theUser.ID, "expiry <": time.Now()}).OrderBy("created_at desc")
+	err = res.One(&token)
+	if err != nil {
+		if err != up.ErrNilRecord && err != up.ErrNoMoreRows {
+			return nil, err
+		}
+	}
+
+	theUser.Token = token
+
+	return &theUser, nil
+}
