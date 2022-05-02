494d7e46d6cd92fc7ebebfecf3e86cc33108a55d
petrostrak <pit.trak@gmail.com>
2022-05-02 21:56:54 +0300

    Add TestGetInsertID

--- data/models_test.go
+++ data/models_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	db2 "github.com/upper/db/v4"
 )
 
 func TestNew(t *testing.T) {
@@ -24,5 +25,21 @@ func TestNew(t *testing.T) {
 
 	if fmt.Sprintf("%T", m) != "data.Models" {
 		t.Error("wrong type", fmt.Sprintf("%T", m))
+	}
+}
+
+func TestGetInsertID(t *testing.T) {
+	var id db2.ID
+
+	id = int64(1)
+	returnedID := getInsertID(id)
+	if fmt.Sprintf("%T", returnedID) != "int" {
+		t.Error("wrong type returned")
+	}
+
+	id = 1
+	returnedID = getInsertID(id)
+	if fmt.Sprintf("%T", returnedID) != "int" {
+		t.Error("wrong type returned")
 	}
 }
