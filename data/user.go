3681fc21573db786bb6ff60a4ba1ffc6283a4cb5
petrostrak <pit.trak@gmail.com>
2022-05-17 19:00:44 +0300

    Add user Validate

--- data/user.go
+++ data/user.go
@@ -6,6 +6,7 @@ import (
 
 	"golang.org/x/crypto/bcrypt"
 
+	"github.com/petrostrak/sokudo"
 	up "github.com/upper/db/v4"
 )
 
@@ -23,6 +24,13 @@ type User struct {
 
 func (u *User) Table() string {
 	return "users"
+}
+
+func (u *User) Validate(validator *sokudo.Validation) {
+	validator.Check(u.LastName != "", "last_name", "Last name must be provided")
+	validator.Check(u.FirstName != "", "first_name", "First name must be provided")
+	validator.Check(u.Email != "", "email", "Email must be provided")
+	validator.IsEmail("email", u.Email)
 }
 
 func (u *User) GetAll() ([]*User, error) {
