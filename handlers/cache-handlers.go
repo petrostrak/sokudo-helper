436173bccf5ad596dfe140cd6cf8799f8b1bbc44
petrostrak <pit.trak@gmail.com>
2022-05-19 13:58:09 +0300

    Implement SaveInCache handler

--- handlers/cache-handlers.go
+++ handlers/cache-handlers.go
@@ -8,7 +8,35 @@ func (h *Handlers) ShowCachePage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (h *Handlers) SaveInCache(w http.ResponseWriter, r *http.Request) {}
+func (h *Handlers) SaveInCache(w http.ResponseWriter, r *http.Request) {
+	var userInput struct {
+		Name  string `json:"name"`
+		Value string `json:"value"`
+		CSRF  string `json:"csrf_token"`
+	}
+
+	err := h.App.ReadJSON(w, r, &userInput)
+	if err != nil {
+		h.App.Error500(w, r)
+		return
+	}
+
+	err = h.App.Cache.Set(userInput.Name, userInput.Value)
+	if err != nil {
+		h.App.Error500(w, r)
+		return
+	}
+
+	var resp struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+
+	resp.Error = false
+	resp.Message = "Saved in cache"
+
+	_ = h.App.WriteJson(w, http.StatusCreated, resp)
+}
 
 func (h *Handlers) GetFromCache(w http.ResponseWriter, r *http.Request) {}
 
